f38425a258275595e113a2ad39577fc5c273a503
Ruman Ahmed Shaikh <35864400+ruman-shaikh@users.noreply.github.com>
2020-09-05 11:33:22 +0530

    Delete ModelList.py

--- DLServer/ModelList.py
+++
@@ -1,9 +0,0 @@
-def ImageNetCatsVDogs(results):
-	classes = {0.0 : "Cat", 1.0 : "Dog"}
-	return classes[results[0][0]]
-
-def PredToStr(ModelName, results):
-	ModelList = {
-		"ImageNetCatsVDogs" : ImageNetCatsVDogs
-	}
-	return ModelList[ModelName](results)
