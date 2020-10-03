1098b7c8477d3824274c37c7345ebd90cef48777
Ruman Ahmed Shaikh <35864400+ruman-shaikh@users.noreply.github.com>
2020-10-03 07:47:37 +0530

    Update client.py

--- DLServer/client.py
+++ DLServer/client.py
@@ -3,16 +3,18 @@ import requests
 import json
 import cv2
 
-addr = 'http://localhost:5000'
+addr = 'http://192.168.0.114:5000'
 test_url = addr + '/api/test'
 
 # prepare headers for http request
+ModelName = 'CatsVDogsKeras'
 content_type = 'image/jpeg'
-headers = {'content-type': content_type}
+headers = {'content-type': content_type, 'modelname': ModelName}
 
 img = cv2.imread('testcat1.jpg')
 # encode image as jpeg
 _, img_encoded = cv2.imencode('.jpg', img)
+print('Waiting for response')
 # send http request with image and receive response
 response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
 # decode response
